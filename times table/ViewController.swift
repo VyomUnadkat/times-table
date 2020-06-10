5e343ce9aab71b7ace6e282f439a2fc29fe84333
VyomUnadkat <18560093+VyomUnadkat@users.noreply.github.com>
2020-06-11 00:28:23 +0530

    initial commit
    slider with times table

--- times table/ViewController.swift
+++ times table/ViewController.swift
@@ -8,8 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource,UITableViewDelegate {
 
+    @IBOutlet weak var slider: UISlider!
+   
+    @IBOutlet weak var table: UITableView!
+    
+    @IBAction func sliderChange(_ sender: Any) {
+        
+        print(Int(slider.value))
+        table.reloadData()
+        
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 50
+    }
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        var cal = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        
+        cal.textLabel?.text = "\(Int(slider.value)) * \(indexPath.row) = \(Int(slider.value) * indexPath.row) "
+        
+        return cal
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
